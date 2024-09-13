4b815bda0f76385edfda886f6d2e7004b805234a
IsaacK47 <isaackayombo41@gmail.com>
2024-09-13 09:07:33 +0200

    Append inputValue funcionality added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// FIREBASE SETUP
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
@@ -5,18 +6,32 @@ const appSettings = {
     databaseURL: "https://playground-526cb-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
+// VARIABLE SECTION
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
-
-
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
+
+//EVENTLISTENER SECTION
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
 
     push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
     
-    console.log(inputValue)
+    appendItemToShoppingListEl(inputValue)
 })
+
+
+// FUNCTION SECTION
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
